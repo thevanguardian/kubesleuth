537e09dd76caa59d4e01a406ae037a0575262c9c
Cody Pate <cody@epic-geek.net>
2024-06-11 22:17:53 -0400

    Test for github actions.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kubesleuth",
-    version="0.1.0",
+    version="0.1.1",
     description="A tool for auditing Kubernetes clusters for misconfigurations, security issues, and best practices.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
