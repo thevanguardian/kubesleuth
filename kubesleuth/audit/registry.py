f3b9af71182d2ec497b61044e5aab84a8a0ce2e0
Cody Pate <cody@epic-geek.net>
2024-06-13 22:53:35 -0400

    Updated task_registry to use a set instead of list

--- kubesleuth/audit/registry.py
+++ kubesleuth/audit/registry.py
@@ -1,17 +1,17 @@
-task_registry = {}
+from collections import defaultdict
+
+task_registry = defaultdict(set) # Assign this as a set, so there are no duplicate task names
 
 def register_category(*categories):
 	def decorator(func):
 		func.categories = categories  # Store categories in the function
 		for category in categories:
-			if category not in task_registry:
-				task_registry[category] = []
-			task_registry[category].append(func)
+			task_registry[category].add(func) # Add category to task_registry set
 		return func
 	return decorator
 
 def get_tasks_by_category(category):
-	return task_registry.get(category, [])
+	return task_registry.get(category, set())
 
 def get_available_categories():
 	return list(task_registry.keys())
